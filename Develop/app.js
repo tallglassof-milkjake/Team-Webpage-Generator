6bc5364b1214b66f9c04324a16f09095e5375045
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-10-25 14:23:31 +1030

    initial prompts created

--- Develop/app.js
+++ Develop/app.js
@@ -10,6 +10,117 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+async function getInfo() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Is there a manager for this team?",
+            name: "role",
+            choices: [
+                "Yes",
+                "No",
+            ]
+        },
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the managers name?"
+        },
+        {
+            type: "input",
+            name: "managerID",
+            message: "What is your ID?"
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is your email address?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your office number?"
+        },
+
+        {
+            type: "list",
+            message: "Are there any more employees to consider?",
+            name: "getEmployee",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        },
+    ]).then(function promptEmployee(info) {
+
+        const manager = info;
+
+        console.log(manager);
+
+        if (manager.getEmployee == "Yes") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "employeeName",
+                    message: "What is the name?"
+                },
+                {
+                    type: "input",
+                    name: "employeeID",
+                    message: "What is your employee ID?"
+                },
+                {
+                    type: "input",
+                    name: "employeeEmail",
+                    message: "What is your Email address?",
+                },
+                {
+                    type: "input",
+                    name: "employeeGithub",
+                    message: "What is your Github username?"
+                },
+                {
+                    type: "list",
+                    message: "Are there any more employees to consider?",
+                    name: "getEmployee",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ]
+                }
+            ]);
+        }
+        
+    });
+}
+
+async function makeItHappen() {
+    getInfo();
+
+}
+
+makeItHappen();
+
+
+// {
+//     type: "list",
+//     message: "Is there any more team members?",
+//     name: "employee",
+//     choices: [
+//         "Yes",
+//         "No"
+//     ]
+// },
+// {
+//     type: "input",
+//     name: "employeeId",
+//     message: "What is your employee ID?"
+// },
+// {
+//     type: "input"
+// }
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
